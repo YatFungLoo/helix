65f80d6324a34bf16275518dcb56fde748c66424
YatFungLoo <looyatfung@gmail.com>
2025-12-11 18:44:54 +0800

    patch: add fallback for mapfile not available
    added if else check to fall back on code from commit f012e84.

--- contrib/completion/hx.bash
+++ contrib/completion/hx.bash
@@ -7,25 +7,47 @@ _hx() {
     cur="${COMP_WORDS[COMP_CWORD]}"
     prev="${COMP_WORDS[COMP_CWORD - 1]}"
 
+    if ! type "mapfile" > /dev/null; then # Source - https://stackoverflow.com/a/7522866
+        MAPFILE_AVAIL=true
+    else
+        MAPFILE_AVAIL=false
+    fi
+
     case "$prev" in
     -g | --grammar)
-        mapfile -t COMPREPLY < <(compgen -W 'fetch build' -- "$cur")
+        if [ "MAPFILE_AVAIL" = true ] ; then
+            mapfile -t COMPREPLY < <(compgen -W 'fetch build' -- "$cur")
+        else
+            COMPREPLY=($(compgen -W 'fetch build' -- "$cur"))
+        fi
         return 0
         ;;
     --health)
         languages=$(hx --health all-languages | tail -n '+2' | awk '{print $1}' | sed 's/\x1b\[[0-9;]*m//g')
-        mapfile -t COMPREPLY < <(compgen -W """clipboard languages all-languages all $languages""" -- "$cur")
+        if [ "MAPFILE_AVAIL" = true ] ; then
+            mapfile -t COMPREPLY < <(compgen -W """clipboard languages all-languages all $languages""" -- "$cur")
+        else
+            COMPREPLY($(compgen -W """clipboard languages all-languages all $languages""" -- "$cur"))
+        fi
         return 0
         ;;
     esac
 
     case "$2" in
     -*)
-        mapfile -t COMPREPLY < <(compgen -W "-h --help --tutor -V --version -v -vv -vvv --health -g --grammar --vsplit --hsplit -c --config --log" -- """$2""")
+        if [ "MAPFILE_AVAIL" = true ] ; then
+            mapfile -t COMPREPLY < <(compgen -W "-h --help --tutor -V --version -v -vv -vvv --health -g --grammar --vsplit --hsplit -c --config --log" -- """$2""")
+        else
+            COMPREPLY($(compgen -W "-h --help --tutor -V --version -v -vv -vvv --health -g --grammar --vsplit --hsplit -c --config --log" -- """$2"""))
+        fi
         return 0
         ;;
     *)
-        mapfile -t COMPREPLY < <(compgen -fd -- """$2""")
+        if [ "MAPFILE_AVAIL" = true ] ; then
+            mapfile -t COMPREPLY < <(compgen -fd -- """$2""")
+        else
+            COMPREPLY($(compgen -fd -- """$2"""))
+        fi
         return 0
         ;;
     esac
